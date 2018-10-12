368e144bc1377667abb7f20694ed1ebef588f986
Matt Ortega <matthew.scott.ortega@gmail.com>
2018-10-12 11:29:54 -0400

    Initial Commit

---
+++ Gen Z Ar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gen Z Ar
+//
+//  Created by Matthew Ortega on 10/12/18.
+//  Copyright © 2018 Matthew Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
